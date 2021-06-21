2787156052a2510d39cf25fb6029b69b38ad77c7
Oleg Korolkov <helkorolkov@gmail.com>
2021-06-22 02:36:31 +0300

    initial check-in of the Bumajechki

---
+++ src/main/java/com/bumajechki/projectbumajechki/ProjectBumajechkiApplication.java
@@ -0,0 +1,13 @@
+package com.bumajechki.projectbumajechki;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjectBumajechkiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ProjectBumajechkiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/bumajechki/projectbumajechki/ProjectBumajechkiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bumajechki.projectbumajechki;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjectBumajechkiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
