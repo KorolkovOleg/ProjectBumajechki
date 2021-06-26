fc6a00c1afad3f87a16acfb6e72a3ce8dcf3edd4
Oleg Korolkov <helkorolkov@gmail.com>
2021-06-26 08:42:43 +0300

    addition login functionality

--- src/main/java/com/bumajechki/projectbumajechki/security/WebSecurityConfig.java
+++ src/main/java/com/bumajechki/projectbumajechki/security/WebSecurityConfig.java
@@ -1,18 +1,27 @@
 package com.bumajechki.projectbumajechki.security;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
 @Configuration
 public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 
+    @Bean
+    public PasswordEncoder getPasswordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+
     @Override
     protected void configure(AuthenticationManagerBuilder auth) throws Exception {
         auth.inMemoryAuthentication()
+                .passwordEncoder(getPasswordEncoder())
                 .withUser("helkorolkov@gmail.com")
-                .password("123")
+                .password(getPasswordEncoder().encode("123"))
                 .roles("USER");
     }
 
